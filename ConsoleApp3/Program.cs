f7917e6119bf6f44ac06d38ca1a9441d8bedcbf2
sulta <sulta@DESKTOP-E785JP2>
2023-10-07 22:06:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строчек");
+            int stroch = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            int stolb = int.Parse(Console.ReadLine());
+            int[,] capa = new int[stroch, stolb];
+            int sum = 0;
+            Random rand = new Random();
+            for (int i = 0; i < stroch; i++)    
+            {
+               for (int j = 0; j < stolb; j++)
+                {
+                    capa[i,j] = rand.Next(10);
+                    Console.Write(capa [i, j]);
+                    sum += capa[i, j];
+                    
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine($"сумма двухмерного массива: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZ.4.8.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ._4._8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrixA = new int[2, 3];
+            int[,] matrixB = new int[2, 3];
+
+            {
+                int numRows = matrixA.GetLength(0);
+                int numCols = matrixA.GetLength(1);
+                Random random = new Random();
+
+                int[,] resultMatrix = new int[numRows, numCols];
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        matrixA[i, j] = random.Next(-9, 10);
+                        matrixA[i, j] = random.Next(-9, 10);
+                        resultMatrix[i, j] = matrixA[i, j] + matrixB[i, j];
+                        Console.WriteLine($"ответ = {resultMatrix[i, j]}");
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
